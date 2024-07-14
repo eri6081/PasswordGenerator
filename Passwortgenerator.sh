150e079fadff997918df65015ab579fd396e1187
Dominik Schweizer <dominikschweizer06@gmail.com>
2024-07-14 17:37:02 +0200

    add help menu

--- Passwortgenerator.sh
+++ Passwortgenerator.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+usage() {
+   echo "Usage: $0 [options]"
+  echo "  -w, --words <num>    Number of words for simple password"
+  echo "  -n, --numbers <num>  Number of numbers for simple password"
+  echo "  -c, --complex <num>  Length of complex password"
+  echo "  -u, --uppercase      Include uppercase letters"
+  echo "  -l, --lowercase      Include lowercase letters"
+  echo "  -s, --special        Include special characters"
+  echo "  -h, --help           Display this help message"
+  exit 0
+}
+
 function EingabeNutzer {
     echo "Bitte wähle eine Option:
           1) Einfaches Passwort generieren
@@ -35,5 +47,9 @@ function KomplexesPasswort {
     password=$(head /dev/urandom | tr -dc 'A-Za-z0-9@#$%^&*' | head -c 20)
     echo "Komplexes Passwort: $password"
 }
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
+fi
 
 EingabeNutzer
